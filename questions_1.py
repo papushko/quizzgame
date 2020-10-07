a04fab27910a75ae5c26f85d9e184dc6ceccb388
Yoni Keren <yoni.keren@gmail.com>
2020-10-07 23:53:49 +0300

    first commit

---
+++ questions_1.py
@@ -0,0 +1,15 @@
+file_name = open('questions.final')
+
+
+for line in file_name:
+    if line.startswith('*'):
+        word = line.split(1:)
+        
+
+
+
+        
+#namesplit=name.split(':')[1]  
+    
+
+
